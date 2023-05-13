105d32356f17e3609c120e70474483f06f37eb0c
yuliyaparamonava <yuliyaparamonava@localhost>
2023-05-13 15:39:22 -0700

    Final Commit

--- src/test/java/hw1/Exercise2YuliyaParamonava.java
+++ src/test/java/hw1/Exercise2YuliyaParamonava.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Exercise2YuliyaParamonava {
 	public static void main(String[] args) {
 
+		// First step
 		System.setProperty("webdriver.chrome.driver", "/Users/yuliyaparamonava/Desktop/B9-Selenium/chromedriver");
 
 		WebDriver driver = new ChromeDriver();
