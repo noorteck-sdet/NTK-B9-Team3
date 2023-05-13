829c20152718a35d8287906d7c91b71129338513
yuliyaparamonava <yuliyaparamonava@localhost>
2023-05-13 14:20:56 -0700

    Commit2

--- src/test/java/hw1/Exercise2YuliyaParamonava.java
+++ src/test/java/hw1/Exercise2YuliyaParamonava.java
@@ -28,69 +28,70 @@ public class Exercise2YuliyaParamonava {
 		String correctPageTitle = "Mercury Tours";
 		if (pageTitle.contains(correctPageTitle)) {
 			System.out.println("Page title verification PASSED");
-		}else {
+		} else {
 			System.out.println("Page title verification FAILED");
 		}
-		
-		//Locate and input FirstName
+
+		// Locate and input FirstName
 		WebElement firstName = driver.findElement(By.name("firstName"));
 		firstName.sendKeys("Yuliya");
-		
-		//Locate and enter LastName
+
+		// Locate and enter LastName
 		WebElement lastName = driver.findElement(By.name("lastName"));
 		lastName.sendKeys("Paramonava");
-		
-		//Locate and input Phone Number 
+
+		// Locate and input Phone Number
 		WebElement phoneNumber = driver.findElement(By.name("phone"));
 		phoneNumber.sendKeys("+18433159107");
-		
-		//Locate and input email
+
+		// Locate and input email
 		WebElement emailAddress = driver.findElement(By.id("userName"));
 		emailAddress.sendKeys("yuliya.paramonava@gmail.com");
-		
-		//Locate and input Address
+
+		// Locate and input Address
 		WebElement address = driver.findElement(By.name("address1"));
 		address.sendKeys("123 Port ave");
-		
-		//Locate and input city
+
+		// Locate and input city
 		WebElement city = driver.findElement(By.name("city"));
 		city.sendKeys("Alexandria");
-		
-		//Locate and input state
+
+		// Locate and input state
 		WebElement state = driver.findElement(By.name("state"));
 		state.sendKeys("VA");
-		
-		//Locate and input ZipCode
+
+		// Locate and input ZipCode
 		WebElement zipCode = driver.findElement(By.name("postalCode"));
 		zipCode.sendKeys("22314");
-		
-		//Locate and input Username
+
+		// Locate and input Username
 		WebElement username = driver.findElement(By.id("email"));
 		username.sendKeys("yuliya.paramonava@gmail.com");
-		
-		//Locate and input Password
+
+		// Locate and input Password
 		WebElement password = driver.findElement(By.name("password"));
 		password.sendKeys("Yuliya123");
-		
-		//Locate and input confirm password
+
+		// Locate and input confirm password
 		WebElement confirmPassword = driver.findElement(By.name("confirmPassword"));
 		confirmPassword.sendKeys("Yuliya123");
-		
-		//Locate and click Submit
+
+		// Locate and click Submit
 		WebElement submitButton = driver.findElement(By.name("submit"));
 		submitButton.click();
-		
-		//Locate and retrieve text from Thank you page
-		WebElement message = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/p[2]/font"));
+
+		// Locate and retrieve text from Thank you page
+		WebElement message = driver.findElement(By.xpath(
+				"/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/p[2]/font"));
 		String strMessage = message.getText();
 		String rightMessage = "Thank you for registering";
 
 		if (strMessage.contains(rightMessage)) {
 			System.out.println("TESTCASE PASSED");
-		}else {
+		} else {
 			System.out.println("TESTCASE FAILED");
 		}
-		
+
 		driver.quit();
 
 	}
